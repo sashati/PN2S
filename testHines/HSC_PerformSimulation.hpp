960cb8cc862bb9022ec2902abf2114cc467b1d56
Saeed <sd.shariati@gmail.com>
2013-11-26 20:04:03 -0200

    Ready to implement Passive

---
+++ testHines/HSC_PerformSimulation.hpp
@@ -0,0 +1,42 @@
+#include "../cudaLibrary/HSC_HinesMatrix.hpp"
+#include "../cudaLibrary/PlatformFunctions.hpp"
+
+//#include "HinesStruct.hpp"
+#include "../cudaLibrary/Connections.hpp"
+#include "../cudaLibrary/SpikeStatistics.hpp"
+#include "../cudaLibrary/NeuronInfoWriter.hpp"
+
+#include <cassert>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <pthread.h>
+
+#ifndef PERFORMSIMULATION_H_
+#define PERFORMSIMULATION_H_
+
+class HSC_PerformSimulation {
+
+private:
+	struct ThreadInfo * tInfo;
+	struct SharedNeuronGpuData *sharedData;
+	struct KernelInfo *kernelInfo;
+
+public:
+    HSC_PerformSimulation(struct ThreadInfo *tInfo);
+    int launchExecution();
+    int setup(Id seed, double dt);
+    int performHostExecution();
+private:
+    void addReceivedSpikesToTargetChannelCPU();
+    void syncCpuThreads();
+    void updateBenchmark();
+    void createNeurons( ftype dt );
+    void createActivationLists( );
+    void initializeThreadInformation();
+    void updateGenSpkStatistics(int *& nNeurons, struct SynapticData *& synData);
+    void generateRandomSpikes(int type, struct RandomSpikeInfo & randomSpkInfo);
+};
+
+#endif /* PERFORMSIMULATION_H_ */
+
