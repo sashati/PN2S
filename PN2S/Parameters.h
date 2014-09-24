c1da466d4c725883ea2b4e830c0026504323be00
Saeed <sd.shariati@gmail.com>
2014-09-24 10:50:02 -0300

    Start network

--- PN2S/Parameters.h
+++ PN2S/Parameters.h
@@ -4,5 +4,5 @@
 #define ARCH_ 	ARCH_SM30
 #define TYPE_	double
 const int DEFAULT_STREAM_NUMBER=1;
-const int MAX_DEVICE_NUMBER=1;
+const int MAX_DEVICE_NUMBER=4;
 
---
+++ PN2S_Moose_Test/network/mpi.sh
@@ -0,0 +1,1 @@
+mpirun -np 1 python single.py : -np 40 ../../../moose
